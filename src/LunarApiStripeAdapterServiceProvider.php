a552c0c104eb7771ffe385b07629c0da4082f738
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-03 17:35:50 +0200

    register custom stripe manager

--- src/LunarApiStripeAdapterServiceProvider.php
+++ src/LunarApiStripeAdapterServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Dystcz\LunarApiStripeAdapter;
 
+use Dystcz\LunarApiStripeAdapter\Managers\StripeManager;
 use Illuminate\Support\ServiceProvider;
 
 class LunarApiStripeAdapterServiceProvider extends ServiceProvider
@@ -9,15 +10,19 @@ class LunarApiStripeAdapterServiceProvider extends ServiceProvider
     /**
      * Bootstrap the application services.
      */
-    public function boot()
+    public function boot(): void
     {
+        $this->app->singleton('gc:stripe', function ($app) {
+            return $app->make(StripeManager::class);
+        });
+
         StripePaymentAdapter::register();
     }
 
     /**
      * Register the application services.
      */
-    public function register()
+    public function register(): void
     {
         // Automatically apply the package configuration
         $this->mergeConfigFrom(__DIR__.'/../config/lunar-api-stripe-adapter.php', 'lunar-api-stripe-adapter');
