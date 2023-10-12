db0b970a56b695fa6afe34cf5ad1bbc5bd6b540a
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-13 00:34:09 +0200

    update config

--- config/lunar-api-stripe-adapter.php
+++ config/lunar-api-stripe-adapter.php
@@ -5,5 +5,18 @@ return [
     'type' => 'card',
     'automatic_payment_methods' => true,
     'payment_method_types' => ['card'],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Capture method policy
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can set whether you want to capture and charge payments
+    | straight away, or create the Payment Intent and release them at a later date.
+    |
+    | automatic - Capture the payment straight away.
+    | manual - Don't take payment straight away and capture later.
+    |
+    */
     'capture_method' => 'automatic',
 ];
