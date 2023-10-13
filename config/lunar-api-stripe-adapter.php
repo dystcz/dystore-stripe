6185ddad60fc6ec770b503408bf159a577c596c7
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-13 16:42:19 +0200

    update config

--- config/lunar-api-stripe-adapter.php
+++ config/lunar-api-stripe-adapter.php
@@ -2,9 +2,18 @@
 
 return [
     'driver' => 'stripe',
+
     'type' => 'stripe',
+
     'automatic_payment_methods' => true,
+
     'payment_method_types' => ['card'],
+
+    'payment_intent_status_map' => [
+        'payment_intent.succeeded' => 'succeeded',
+        'payment_intent.canceled' => 'canceled',
+        'payment_intent.payment_failed' => 'failed',
+    ],
 
     /*
     |--------------------------------------------------------------------------
