0b516495ab9c3fada78b073a1385b6e47a1a44c5
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-01-18 05:20:21 +0100

    handle webhooks on queue

---
+++ src/Jobs/HandleWebhook.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Dystcz\LunarApiStripeAdapter\Jobs;
+
+use Dystcz\LunarApi\Domain\Orders\Events\OrderPaymentCanceled;
+use Dystcz\LunarApi\Domain\Orders\Events\OrderPaymentFailed;
+use Dystcz\LunarApi\Domain\Payments\PaymentAdapters\PaymentIntent;
+use Dystcz\LunarApiStripeAdapter\Actions\AuthorizeStripePayment;
+use Dystcz\LunarApiStripeAdapter\StripePaymentAdapter;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
+use Lunar\Models\Cart;
+use Stripe\Event;
+use Stripe\Exception\SignatureVerificationException;
+use Stripe\Webhook;
+use UnexpectedValueException;
+
+class HandleWebhook implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public StripePaymentAdapter $adapter;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        public array $data,
+        public string $payload,
+        public string $signature,
+    ) {
+        $this->adapter = new StripePaymentAdapter;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $event = $this->constructEvent();
+
+        $stripePaymentIntent = $event->data->object;
+
+        $statusMap = Config::get('lunar-api.stripe.payment_intent_status_map', []);
+
+        $paymentIntentStatus = match (true) {
+            in_array($event->type, array_keys($statusMap)) => $statusMap[$event->type],
+            default => 'unknown',
+        };
+
+        $paymentIntent = new PaymentIntent(
+            id: $stripePaymentIntent->id,
+            amount: $stripePaymentIntent->amount,
+            status: $paymentIntentStatus,
+            client_secret: $stripePaymentIntent->client_secret,
+        );
+
+        try {
+            $cart = Cart::query()
+                ->with(['draftOrder', 'completedOrder'])
+                ->where('meta->payment_intent', $paymentIntent->id)
+                ->firstOrFail();
+        } catch (\Exception $e) {
+            $this->fail($e);
+        }
+
+        /** @var Order $order */
+        $order = $cart->draftOrder ? $cart->draftOrder : $cart->completedOrder;
+
+        switch ($paymentIntentStatus) {
+            case 'succeeded':
+                App::make(AuthorizeStripePayment::class)($order, $cart, $paymentIntent);
+                break;
+            case 'canceled':
+                OrderPaymentCanceled::dispatch($order, $this->adapter, $paymentIntent);
+                break;
+            case 'failed':
+                OrderPaymentFailed::dispatch($order, $this->adapter, $paymentIntent);
+                break;
+            default:
+                Log::info('Received unknown event type '.$event->type);
+        }
+    }
+
+    /**
+     * Construct Stripe event.
+     */
+    protected function constructEvent(): Event
+    {
+        if (App::environment('testing')) {
+            return Event::constructFrom($this->data);
+        }
+
+        try {
+            return Webhook::constructEvent(
+                $this->payload,
+                $this->signature,
+                Config::get('services.stripe.webhooks.payment_intent'),
+            );
+        } catch (UnexpectedValueException|SignatureVerificationException $e) {
+            $this->fail($e);
+        }
+    }
+}
--- src/StripePaymentAdapter.php
+++ src/StripePaymentAdapter.php
@@ -2,23 +2,14 @@
 
 namespace Dystcz\LunarApiStripeAdapter;
 
-use Dystcz\LunarApi\Domain\Orders\Events\OrderPaymentCanceled;
-use Dystcz\LunarApi\Domain\Orders\Events\OrderPaymentFailed;
 use Dystcz\LunarApi\Domain\Payments\PaymentAdapters\PaymentAdapter;
 use Dystcz\LunarApi\Domain\Payments\PaymentAdapters\PaymentIntent;
-use Dystcz\LunarApiStripeAdapter\Actions\AuthorizeStripePayment;
+use Dystcz\LunarApiStripeAdapter\Jobs\HandleWebhook;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Log;
 use Lunar\Models\Cart;
-use Lunar\Models\Order;
 use Lunar\Stripe\Facades\StripeFacade;
-use Stripe\Event;
-use Stripe\Exception\SignatureVerificationException;
-use Stripe\Webhook;
-use UnexpectedValueException;
 
 class StripePaymentAdapter extends PaymentAdapter
 {
@@ -63,86 +54,12 @@ class StripePaymentAdapter extends PaymentAdapter
      */
     public function handleWebhook(Request $request): JsonResponse
     {
-        $event = App::environment('testing')
-            // Construct directly from request in testing environment
-            ? Event::constructFrom($request->all())
-            // Construct from Stripe webhook while checking signature
-            : $this->constructEvent($request);
-
-        // Return early if event counld not be constructed
-        if ($event instanceof JsonResponse) {
-            return $event;
-        }
-
-        $stripePaymentIntent = $event->data->object;
-
-        $statusMap = Config::get('lunar-api.stripe.payment_intent_status_map', []);
-
-        $paymentIntentStatus = match (true) {
-            in_array($event->type, array_keys($statusMap)) => $statusMap[$event->type],
-            default => 'unknown',
-        };
-
-        $paymentIntent = new PaymentIntent(
-            id: $stripePaymentIntent->id,
-            amount: $stripePaymentIntent->amount,
-            status: $paymentIntentStatus,
-            client_secret: $stripePaymentIntent->client_secret,
+        HandleWebhook::dispatch(
+            data: $request->all(),
+            payload: $request->getContent(),
+            signature: $request->header('Stripe-Signature'),
         );
 
-        $cart = Cart::query()
-            ->with(['draftOrder', 'completedOrder'])
-            ->where('meta->payment_intent', $paymentIntent->id)
-            ->first();
-
-        if (! $cart) {
-            return new JsonResponse([
-                'webhook_successful' => false,
-                'message' => "Cart not find cart with payment_intent: {$paymentIntent->id}",
-            ], 200);
-        }
-
-        /** @var Order $order */
-        $order = $cart->draftOrder ? $cart->draftOrder : $cart->completedOrder;
-
-        switch ($paymentIntentStatus) {
-            case 'succeeded':
-                App::make(AuthorizeStripePayment::class)($order, $cart, $paymentIntent);
-                break;
-            case 'canceled':
-                OrderPaymentCanceled::dispatch($order, $this, $paymentIntent);
-                break;
-            case 'failed':
-                OrderPaymentFailed::dispatch($order, $this, $paymentIntent);
-                break;
-            default:
-                Log::info('Received unknown event type '.$event->type);
-        }
-
-        return new JsonResponse([
-            'webhook_successful' => true,
-            'message' => 'Webook handled successfully',
-        ]);
-    }
-
-    /**
-     * Construct Stripe event.
-     */
-    protected function constructEvent(Request $request): JsonResponse|Event
-    {
-        try {
-            return Webhook::constructEvent(
-                $request->getContent(),
-                $request->header('Stripe-Signature'),
-                Config::get('services.stripe.webhooks.payment_intent')
-            );
-        } catch (UnexpectedValueException|SignatureVerificationException $e) {
-            report($e);
-
-            return new JsonResponse([
-                'webhook_successful' => false,
-                'message' => $e->getMessage(),
-            ], 400);
-        }
+        return new JsonResponse(null, 200);
     }
 }
