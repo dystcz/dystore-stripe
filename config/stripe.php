40ce4cbd223357330cf76b5a1eac4c16d6553809
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-02-09 13:03:58 +0100

    update config

--- config/stripe.php
+++ config/stripe.php
@@ -1,6 +1,25 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Sripe payment driver
+    |--------------------------------------------------------------------------
+    |
+    | Drivers for lunar are set in lunar.payments.types.
+    | When stripe is set as a driver, this adapter will be used.
+    |
+    */
     'driver' => 'stripe',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sripe payment type
+    |--------------------------------------------------------------------------
+    |
+    | This key serves is an identification for this adapter.
+    | That means that stripe driver is handled by this adapter if configured.
+    |
+    */
     'type' => 'stripe',
 ];
